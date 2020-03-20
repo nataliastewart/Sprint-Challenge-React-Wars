666a010ea84352abe68f0532937b63b61836164e
nataliastewart <nataliastewart@users.noreply.github.com>
2020-03-20 11:57:40 -0400

    Axios rerturning the data normally

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
+        console.log("APP RESPONSE: ", response.data);
+      })
+      .catch(error => {
+        console.log("Data was not returned from axios!!!", error);
+      });
+  }, []);
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
@@ -14,6 +27,6 @@ const App = () => {
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
