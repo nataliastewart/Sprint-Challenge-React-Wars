c93ed2f20e1184db17d3561994210f629084a100
nataliastewart <nataliastewart@users.noreply.github.com>
2020-03-20 13:18:58 -0400

    Styled components Done - working normally - Cards ready

--- starwars/src/PeopleCard.js
+++ starwars/src/PeopleCard.js
@@ -1,14 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+
+//styled components
+
+const Container = styled.div`
+  border: 2px solid black;
+  width: 20%;
+  margin: 0 auto;
+  box-sizing: border-box;
+  margin-bottom: 2%;
+  border-radius: 10px;
+  background-color: #ffffff;
+  opacity: 0.6;
+  boxsizing: "border-box";
+`;
+
+// const PageContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: space-between;
+// `;
+
+const TitleCard = styled.div`
+  border: 2px solid black;
+  width: 20%;
+  margin: 0 auto;
+  box-sizing: border-box;
+  border-radius: 10px;
+  font-size: 2rem;
+  background-color: #4d3319;
+  color: #f9f2ec;
+`;
+// border: 3px solid black;
 
 const PeopleCard = props => {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.heigth}</h3>
-      <h3>{props.eyeColor}</h3>
-      <h3>{props.skinColor}</h3>
-      <h3>{props.mass}</h3>
-    </div>
+    <>
+      <TitleCard>{props.name}</TitleCard>
+      <Container>
+        <h3>Height: {props.height}</h3>
+        <h3>Eye Color: {props.eyeColor}</h3>
+        <h3>Skin Color: {props.skinColor}</h3>
+        <h3>Mass: {props.mass}</h3>
+      </Container>
+    </>
   );
 };
 
